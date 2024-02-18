f924b93f80414ef942bf3409081069b55e4bc778
Danofe <80469462+Danofe@users.noreply.github.com>
2024-02-18 01:19:22 +0100

    Fikset generator UI og Resultat UI (Bare en start) Laget en object for BrukerData

---
+++ VismaBlazor/VismaBlazor.Client/Models/Bruker.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace VismaBlazor.Client.Models
+{
+    public class Bruker
+    {
+
+        public string Fornavn { get; set; } = string.Empty;
+        public string Etternavn { get; set; } = string.Empty;
+        public string Epost { get; set; } = string.Empty;
+        public int Telefon { get; set; }
+        public string NøkkelID { get; set; } = string.Empty;
+
+        public string Adresse { get; set; } = string.Empty;
+
+    }
+}
