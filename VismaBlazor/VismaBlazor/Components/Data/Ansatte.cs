3359c75abf06a247a84c6c21708e41e60be477d7
Philip Toan Nguyen <Philip2an@hotmail.com>
2024-02-24 02:40:51 +0100

    Prøvd å ordne Json deserialize. Får ikke til konsoll.

---
+++ VismaBlazor/VismaBlazor/Components/Data/Ansatte.cs
@@ -0,0 +1,14 @@
+﻿namespace VismaBlazor.Components.Data
+{
+    public class Ansatte
+    {
+        public int Id { get; set; }
+        public string Fornavn { get; set; } = string.Empty;
+        public string Etternavn { get; set; } = string.Empty;
+        public string EPost { get; set; } = string.Empty;
+        public string Brukernavn { get; set; } = string.Empty;
+        public string Passord { get; set; } = string.Empty;
+
+
+    }
+}
