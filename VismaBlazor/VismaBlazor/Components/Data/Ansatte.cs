0ed0062eeca5691509544e2dafd164787014d179
Philip Toan Nguyen <Philip2an@hotmail.com>
2024-04-07 00:30:44 +0200

    Importer og Ansatte.cs er oppdatert og fikset

--- VismaBlazor/VismaBlazor/Components/Data/Ansatte.cs
+++ VismaBlazor/VismaBlazor/Components/Data/Ansatte.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string Fornavn { get; set; } = string.Empty;
         public string Etternavn { get; set; } = string.Empty;
-        public string EPost { get; set; } = string.Empty;
+        public int Telefon { get; set; } 
         public string Brukernavn { get; set; } = string.Empty;
         public string Passord { get; set; } = string.Empty;
-
-
-    }
+        public string EPost { get; set; } = string.Empty;
+    
+}
 }
