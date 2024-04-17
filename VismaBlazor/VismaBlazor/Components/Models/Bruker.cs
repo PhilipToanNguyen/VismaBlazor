83ad0ca9efd8786c6aafb648337e721a86fc1e28
Philip Toan Nguyen <Philip2an@hotmail.com>
2024-04-17 20:21:08 +0200

    endringer

--- VismaBlazor/VismaBlazor/Components/Models/Bruker.cs
+++ VismaBlazor/VismaBlazor/Components/Models/Bruker.cs
@@ -7,11 +7,10 @@ namespace VismaBlazor.Models
 
         public string Fornavn { get; set; } = string.Empty;
         public string Etternavn { get; set; } = string.Empty;
-        public int Tlf { get; set; }
-        public int Id { get; set; }
+        public string Tlf { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
         public string Epost { get; set; } = string.Empty;
         public string Brukernavn { get; set; } = string.Empty;
-
         public string Passord { get; set; } = string.Empty;
 
     }
