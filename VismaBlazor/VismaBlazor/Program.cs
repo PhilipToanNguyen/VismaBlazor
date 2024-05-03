685543e767fc5cb56072f990f3aedbedc222fe87
Danofe <80469462+Danofe@users.noreply.github.com>
2024-05-03 16:35:29 +0200

    hide configs på nytt

--- VismaBlazor/VismaBlazor/Program.cs
+++ VismaBlazor/VismaBlazor/Program.cs
@@ -10,10 +10,10 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Environment.SetEnvironmentVariable("Audience", builder.Configuration.GetSection("Auth0").GetSection("Audience").Value);
+/* Environment.SetEnvironmentVariable("Audience", builder.Configuration.GetSection("Auth0").GetSection("Audience").Value);
 Environment.SetEnvironmentVariable("Domain", builder.Configuration.GetSection("Auth0").GetSection("Domain").Value);
 Environment.SetEnvironmentVariable("ClientId", builder.Configuration.GetSection("Auth0").GetSection("ClientId").Value);
-Environment.SetEnvironmentVariable("ClientSecret", builder.Configuration.GetSection("Auth0").GetSection("ClientSecret").Value);
+Environment.SetEnvironmentVariable("ClientSecret", builder.Configuration.GetSection("Auth0").GetSection("ClientSecret").Value); */
 
 var audience = Environment.GetEnvironmentVariable("Audience");
 var domain = Environment.GetEnvironmentVariable("Domain");
@@ -35,8 +35,8 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
     //options.Domain = domain;
     //options.ClientId = clientid;
 
-    options.Domain = builder.Configuration["Auth0:Domain"];
-    options.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.Domain = domain;
+    options.ClientId = clientid;
 });
 
 builder.Services.AddCascadingAuthenticationState();
