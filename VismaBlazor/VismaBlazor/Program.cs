dafb61ecffce60658d9133cea04096d3d5ee265a
Danofe <80469462+Danofe@users.noreply.github.com>
2024-05-13 18:47:27 +0200

    NÆRMESTE VI KAN BLI

--- VismaBlazor/VismaBlazor/Program.cs
+++ VismaBlazor/VismaBlazor/Program.cs
@@ -11,15 +11,15 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Environment.SetEnvironmentVariable("Audience", builder.Configuration.GetSection("Auth0").GetSection("Audience").Value);
-//Environment.SetEnvironmentVariable("Domain", builder.Configuration.GetSection("Auth0").GetSection("Domain").Value);
-//Environment.SetEnvironmentVariable("ClientId", builder.Configuration.GetSection("Auth0").GetSection("ClientId").Value);
-//Environment.SetEnvironmentVariable("ClientSecret", builder.Configuration.GetSection("Auth0").GetSection("ClientSecret").Value);
+Environment.SetEnvironmentVariable("Audience", builder.Configuration.GetSection("Auth0").GetSection("Audience").Value);
+Environment.SetEnvironmentVariable("Domain", builder.Configuration.GetSection("Auth0").GetSection("Domain").Value);
+Environment.SetEnvironmentVariable("ClientId", builder.Configuration.GetSection("Auth0").GetSection("ClientId").Value);
+Environment.SetEnvironmentVariable("ClientSecret", builder.Configuration.GetSection("Auth0").GetSection("ClientSecret").Value);
 
-var audience = Environment.GetEnvironmentVariable("Audience");
-var domain = Environment.GetEnvironmentVariable("Domain");
-var clientid = Environment.GetEnvironmentVariable("ClientId");
-var clientsecret = Environment.GetEnvironmentVariable("ClientSecret");
+//var audience = Environment.GetEnvironmentVariable("Audience");
+//var domain = Environment.GetEnvironmentVariable("Domain");
+//var clientid = Environment.GetEnvironmentVariable("ClientId");
+//var clientsecret = Environment.GetEnvironmentVariable("ClientSecret");
 
 
 
@@ -33,11 +33,11 @@ builder.Services.AddRazorComponents()
 //Add services for authentication
 builder.Services.AddAuth0WebAppAuthentication(options =>
 {
-     options.Domain = domain;
-     options.ClientId = clientid;
+    // options.Domain = domain;
+    // options.ClientId = clientid;
 
-    //options.Domain = builder.Configuration["Auth0:Domain"];
-    //options.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
 
 });
 
