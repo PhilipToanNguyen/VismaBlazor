1e5ce25cedb132002ff6a5d68313a4e78bf76dc4
Philip Toan Nguyen <Philip2an@hotmail.com>
2024-05-15 01:59:07 +0200

    Litt og litt forandringer.

--- VismaBlazor/VismaBlazor/wwwroot/js/saveFile.js
+++ VismaBlazor/VismaBlazor/wwwroot/js/saveFile.js
@@ -1,13 +1,13 @@
-﻿window.saveAsFile = (fileName, content) => {
-    const blob = new Blob([content], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = fileName;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-
+﻿window.saveAsFile = (filNavn, innhold) => {
+    const blobObjekt = new Blob([innhold], { type: 'application/json' }); //Oppretter objekt som vil evt være dataen jeg vil lagre
+    const url = URL.createObjectURL(blobObjekt); //brukes for å referere dataen jeg ønsker å laste ned
+    const link = document.createElement('a'); //oppretter link element som tar initiativ til nedlastning 
+    link.href = url;
+    link.download = filNavn;
+    document.body.appendChild(link);//linken blir lagt til kroppen
+    link.click(); //ved klikk startes nedlastning
+    document.body.removeChild(link); //etter klikk, så vil linken løse fra seg
     URL.revokeObjectURL(url);
 };
+
+
